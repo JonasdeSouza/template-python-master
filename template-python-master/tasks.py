881689b74082732ef1223f6066afff3bf1f92770
JonasDeSouza <jonasdesouza28@gmail.com>
2024-07-23 22:58:10 -0300

    funciona

--- template-python-master/tasks.py
+++ template-python-master/tasks.py
@@ -1,18 +1,17 @@
 from robocorp.tasks import task
-from robocorp.workitems import WorkItems
+from robocorp import workitems 
 
 @task
 def main():
-    workitems = WorkItems()
-    input_item = workitems.get_input_work_item()
-    payload = input_item.payload
+    item = workitems.inputs.current
+    payload = item.payload
     
     # Assuming the payload is a dictionary that contains the arguments you need
     arg1 = payload.get("number")
     arg2 = payload.get("alo")
     minimal_task(arg1, arg2)
 
-    
+
 def minimal_task(number, alo):
     message = "Hello"
     message = message + " World!"
